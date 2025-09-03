1023b0166de58a8381354dd2827e00e4df86d66f
JahongirTRLI <jahongir.trli@gmail.com>
2025-09-03 15:39:36 +0500

    fix requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,5 +152,4 @@ answer = st.sidebar.text_area(
 if st.sidebar.button("Найти"):
     model = build_model(df)
     result = find_similar_task(df, model, answer, topn=5)
-    st.dataframe(result)
-
+    show_B(result)
