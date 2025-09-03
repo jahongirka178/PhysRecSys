7eb4ff0ce590961b921ea05b2680d6d8d5dfcf96
JahongirTRLI <jahongir.trli@gmail.com>
2025-09-03 06:08:55 +0500

    Update problems.csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import pandas as pd
 import streamlit as st
 
 
+st.title('🎈 Задачник')
+
+
 DATA_PATH = 'https://raw.githubusercontent.com/jahongirka178/PhysRecSys/refs/heads/master/problems.csv'
 
 df = pd.read_csv(DATA_PATH)
 
-st.title('🎈 App Name')
-
 st.write(df['Task'].iloc[1])
 
-st.write('Hello world!')
