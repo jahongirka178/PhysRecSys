2e7dc74081595322ba99710630f11ad8b8adf36b
JahongirTRLI <jahongir.trli@gmail.com>
2025-09-03 05:42:31 +0500

    Add data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
+import pandas as pd
 import streamlit as st
 
+
+DATA_PATH = 'https://raw.githubusercontent.com/jahongirka178/PhysRecSys/refs/heads/master/problems.csv'
+
+df = pd.read_csv(DATA_PATH)
+
 st.title('🎈 App Name')
+
+st.write(df['Task'].iloc[1])
 
 st.write('Hello world!')
